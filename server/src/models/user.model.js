8a4a5b502c1cb4665f4dc3bcc99f827e90db98d8
Bitan Banerjee <bbitan244@gmail.com>
2026-06-20 12:47:13 +0530

    Add User model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      min: [5, "Password must be Minimum 5 characters"],
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
