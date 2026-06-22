eb5a24e2d389a05208f1f4c1cf13040c554f9706
Bitan Banerjee <bbitan244@gmail.com>
2026-06-22 15:50:15 +0530

    Adding Logout Feature

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,10 +1,31 @@
 import { useState } from "react";
-import { Coins } from "lucide-react";
-import { useSelector } from "react-redux";
+import { Coins, CoinsIcon, LogOut } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { setUserData } from "../store/user/userSlice";
+import { toast } from "react-hot-toast";
 
 function Nav({ setOpen, setOpenSignUp }) {
   // Hooks
   const user = useSelector((store) => store.user.userData);
+  const [popUp, setPopUp] = useState(true);
+  let dispatch = useDispatch();
+
+  // Functions and State
+  const logOutUser = async () => {
+    try {
+      let response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/api/v2/logout`,
+        { withCredentials: true },
+      );
+      dispatch(setUserData(null));
+      toast.success(response?.data?.message);
+      setPopUp(prev=>!prev);
+    } catch (error) {
+      console.log(error.response?.data?.message);
+      toast.error(error?.response?.data?.message);
+    }
+  };
 
   return (
     <nav className="w-full h-14 z-50 bg-black text-white fixed top-0 left-0 border-b border-gray-800">
@@ -40,19 +61,71 @@ function Nav({ setOpen, setOpenSignUp }) {
             </>
           )}
 
-        
           {user && user?.avatar == "" && (
-            <div className="w-8 h-8 sm:w-9 sm:h-9 rounded-full bg-gray-800 border border-gray-700 flex justify-center items-center cursor-pointer">
+            <div
+              className="w-8 h-8 sm:w-9 sm:h-9 rounded-full bg-gray-800 border border-gray-700 flex justify-center items-center cursor-pointer"
+              onClick={() => setPopUp((prev) => !prev)}
+            >
               {user?.fullName[0]}
             </div>
           )}
 
           {user && user?.avatar != "" && (
             <img
-              className="w-8 h-8 sm:w-9 sm:h-9 rounded-full bg-gray-800 border border-gray-700 flex justify-center items-center cursor-pointer"
+              className={`w-8 h-8 sm:w-9 sm:h-9 rounded-full bg-gray-800 border border-gray-700 flex justify-center items-center cursor-pointer`}
               src={user?.avatar}
+              onClick={() => setPopUp((prev) => !prev)}
             />
           )}
+
+          <div
+            className={`${
+              popUp ? "hidden" : "block"
+            } z-10 absolute top-[60px] right-7 w-54 bg-black rounded-xl shadow-lg border border-gray-100/30 overflow-hidden `}
+          >
+            {/* Profile Section */}
+            <div className="p-4 flex items-center gap-3 border border-solid border-b-2 border-gray-100/30">
+              {user && user?.avatar == "" && (
+                <div
+                  className="w-8 h-8 sm:w-9 sm:h-9 rounded-full bg-gray-800 border border-gray-700 flex justify-center items-center cursor-pointer"
+                  onClick={() => setPopUp((prev) => !prev)}
+                >
+                  {user?.fullName[0]}
+                </div>
+              )}
+
+              {user && user?.avatar != "" && (
+                <img
+                  className={`w-8 h-8 sm:w-9 sm:h-9 rounded-full bg-gray-800 border border-gray-700 flex justify-center items-center cursor-pointer`}
+                  src={user?.avatar}
+                  onClick={() => setPopUp((prev) => !prev)}
+                />
+              )}
+              <div>
+                <p className="font-semibold">{user?.fullName}</p>
+              </div>
+            </div>
+
+            {/* Credit Section */}
+            <div className="p-4 flex justify-between items-center">
+              <span className="flex gap-2">
+                <CoinsIcon className="text-orange-300" size={18} />
+                Credits
+              </span>
+              <span className="font-bold">1000</span>
+            </div>
+
+            {/* Logout */}
+            <div className="p-3">
+              <button
+                className="w-full bg-red-700 hover:bg-red-900 text-white py-2 rounded-lg transition flex justify-center items-center gap-2"
+                onClick={logOutUser}
+              >
+                <LogOut size={18} />
+                Logout
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
